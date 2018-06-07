70b78c92b0a2718bacc77034855be83ed9a5cb62
Gonzalo Guzman <gonzaguz@ucm.es>
2018-06-08 00:48:31 +0200

    Práctica 5 IA

---
+++ IAVP5-BD-TPC/Assets/Scripts/collisionWithGhosts.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.ThirdPersonController.Wrappers;
+public class collisionWithGhosts : MonoBehaviour {
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider info)
+    {
+        if (info.CompareTag("Ghost"))
+        {
+            GetComponent<CharacterHealth>().Damage(5, this.transform.position, Vector3.zero);
+        }
+    }
+}
